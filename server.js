e2aebb632e9be8697b6b49c75112e8ad0de2424f
woongcloud <tjsdnd3103@naver.com>
2023-03-03 18:44:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.listen(8080, function(){
     console.log('listening on 8080')
 });
 
-// 누군가가 /pet 으로 방문을 하면..
-// pet관련된 안내문을 띄워주자
-// 커밋 테스트용입니다.
+// 누군가가 /?? 으로 방문을 하면..
+// ??관련된 안내문을 띄워주자
+
 
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html')
