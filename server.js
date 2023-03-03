32e8d23b165b503085a6bdadd953d1a2daa49984
woongcloud <tjsdnd3103@naver.com>
2023-03-03 16:38:37 +0900

    서버 개설/ 홈페이지 생성
    23.03.03

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+app.listen(8080, function(){
+    console.log('listening on 8080')
+});
+
+// 누군가가 /pet 으로 방문을 하면..
+// pet관련된 안내문을 띄워주자
+// 커밋 테스트용입니다.
+
+app.get('/', function(요청, 응답){
+    응답.sendFile(__dirname + '/index.html')
+
+});
+
+ app.get('/write', function(요청, 응답){
+    응답.sendFile(__dirname + '/write.html');
+ }); 
+
+ //어떤 사람이 /add 경로로 POST 요청을 하면...
+ //?? 를 해주세요~
+
+app.post('/add', function(요청, 응답){
+    응답.send('전송완료')
+    console.log(요청.body.title)
+});
