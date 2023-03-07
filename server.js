4289aee4684ef01957eeacbf863053b884f761cc
sunwoongJang <tjsdnd3103@naver.com>
2023-03-08 03:08:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
-app.listen(8080, function(){
-    console.log('listening on 8080')
-});
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://tjsdnd3103:<dkssud159>@sunwoong.pbgylxn.mongodb.net/?retryWrites=true&w=majority', 
+function(에러, client){
+
+    app.listen(8080, function(){
+        console.log('listening on 8080')
+    });
+
+})
+
+
+
+
+
 
 // 누군가가 /?? 으로 방문을 하면..
 // ??관련된 안내문을 띄워주자
@@ -23,7 +35,7 @@ app.get('/', function(요청, 응답){
  //?? 를 해주세요~
 
 app.post('/add', function(요청, 응답){
-    응답.send('전송완료')
+    응답.send('전송완료!!!')
     console.log(요청.body.title)
     console.log(요청.body.date)
 });
