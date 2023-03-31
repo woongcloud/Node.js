603e2003ad1f11997926069bd03f00547d363181
sunwoongJang <tjsdnd3103@naver.com>
2023-03-31 18:15:17 +0900

    AJAX
    AJAX 이용한 delete기능 추가/ html수정

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ MongoClient.connect('mongodb+srv://tjsdnd3103:tjsdnd3103@sunwoong.pbgylxn.mongod
 // ??관련된 안내문을 띄워주자
 
 
-app.get('/', function(요청, 응답){
+app.get('/', function(요청, 응답){ // '/'는 기본값홈페이지로 지정함
     응답.sendFile(__dirname + '/index.html')
 
 });
@@ -45,13 +45,32 @@ app.get('/', function(요청, 응답){
 
 app.post('/add', function(요청, 응답){
     응답.send('전송완료!!!')
-    console.log(요청.body.title)
-    console.log(요청.body.date)
-    db.collection('post').insertOne( { 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
-        console.log('저장완료')
-      });
+    db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
+        console.log(결과.totalPost)
+        var 총게시물갯수 = 결과.totalPost;
+
+        db.collection('post').insertOne( { _id : 총게시물갯수 + 1, 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
+            console.log('저장완료');
+            //counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜야함 (수정)
+            db.collection('counter').updateOne({name:'게시물갯수'},{ $inc : {totalPost:1} },function(에러, 결과){
+                if(에러){return console.log(에러)}
+            })
+          });
+
+    
+
+
+    });
+    
 });
- 
+
+
+
+
+
+
+
+
 ///list로 GET요청으로 접속하면
 //실제 DB에 저장된 데이터들로 예쁘게 꾸며진HTML을 보여줌
 
@@ -67,3 +86,12 @@ app.get('/list', function(요총, 응답){
     
 });
 
+app.delete('/delete', function(요청, 응답){
+    console.log(요청.body)
+    요청.body._id = parseInt(요청.body._id);
+    //요청.body에 담겨온 게시물번호를 가진 글을 db에서 찾아서 삭제해주세요
+    db.collection('post').deleteOne(요청.body, function(에러, 결과){
+        console.log('삭제완료');
+        응답.status(200).send({ messsage : '성공했습니다' });
+    });
+});
